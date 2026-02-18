d51328b9ee83bf5c5c634a21044adab8df49d890
VaibhavGawass <148565752+VaibhavGawass@users.noreply.github.com>
2026-02-18 21:55:00 +0530

    Feature/admin desk management (#1)
    * Add desk model class
    
    * Add desk dao interface
    
    * Add jdbcdeskdao class that implements the desk dao class
    
    * Add the DeskService validations (unique code, required fields,
    deactivate)
    
    * feat: DeskController admin menu and wiring
    
    * Update the app config class to add desk controller
    
    * Update app class to add menu option for desk controller

--- src/main/java/com/deskmate/config/AppConfig.java
+++ src/main/java/com/deskmate/config/AppConfig.java
@@ -1,6 +1,15 @@
 package com.deskmate.config;
 
-public class AppConfig {
+import com.deskmate.controller.DeskController;
+import com.deskmate.dao.DeskDao;
+import com.deskmate.dao.impl.JdbcDeskDao;
+import com.deskmate.services.DeskService;
 
-    
+public class AppConfig {
+	public DeskController deskController() {
+        DeskDao deskDao = new JdbcDeskDao();
+        DeskService deskService = new DeskService(deskDao);
+        return new DeskController(deskService);
+    }
+
 }
