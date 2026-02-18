1a8778f41f8cb7118f241141f9ef277015efcebf
Vaibhav Gawas <vaibhavgawas343@gmail.com>
2026-02-18 23:03:17 +0530

    Wire BookingService with JdbcBookingDao and JdbcPaymentDao in AppConfig

--- src/main/java/com/deskmate/config/AppConfig.java
+++ src/main/java/com/deskmate/config/AppConfig.java
@@ -1,8 +1,14 @@
 package com.deskmate.config;
 
+import com.deskmate.controller.BookingController;
 import com.deskmate.controller.DeskController;
+import com.deskmate.dao.BookingDao;
 import com.deskmate.dao.DeskDao;
+import com.deskmate.dao.PaymentDao;
+import com.deskmate.dao.impl.JdbcBookingDao;
 import com.deskmate.dao.impl.JdbcDeskDao;
+import com.deskmate.dao.impl.JdbcPaymentDao;
+import com.deskmate.services.BookingService;
 import com.deskmate.services.DeskService;
 
 public class AppConfig {
@@ -10,6 +16,14 @@ public class AppConfig {
         DeskDao deskDao = new JdbcDeskDao();
         DeskService deskService = new DeskService(deskDao);
         return new DeskController(deskService);
+    }
+
+	public BookingController bookingController() {
+        DeskDao deskDao = new JdbcDeskDao();
+        BookingDao bookingDao = new JdbcBookingDao();
+        PaymentDao paymentDao = new JdbcPaymentDao();
+        BookingService bookingService = new BookingService(deskDao, bookingDao, paymentDao);
+        return new BookingController(bookingService);
     }
 
 }
